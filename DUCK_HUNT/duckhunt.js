5fea8ecc0f5ab521c9471b2b11983a5529d1d415
ChicoMangusta <andreptorres@gmail.com>
2020-04-03 02:18:22 -0400

    TIMER COMMIt

--- DUCK_HUNT/duckhunt.js
+++ DUCK_HUNT/duckhunt.js
@@ -1,66 +1,66 @@
 // //doc ready  
-// $(() => {
-//     const body = $("body");
-//     console.log(body); 
+$(() => {
+    const body = $("body");
+    console.log(body); 
 
-//     //CREATING THE DUCK
-//     const createDuck = () => {
-//         //CREATE A NEW DIV WITH A CLASS OF DUCK
-//         let $newDuck = $("<div>").addClass("duck");
+    //CREATING THE DUCK
+    const createDuck = () => {
+        //CREATE A NEW DIV WITH A CLASS OF DUCK
+        let $newDuck = $("<div>").addClass("duck");
 
-//         // APPENDING THE NEW DIV TO THE BODY
-//         $("body").append($newDuck);
-//          console.log('duck')
+        // APPENDING THE NEW DIV TO THE BODY
+        $("body").append($newDuck);
+         console.log('duck')
 
-//         //CREATING THE DUCKS WINGS FLAPS
-//         setInterval(function () {
-//             $newDuck.classList.toggle("flap");
-//         }, 200);
-//     };
+        //CREATING THE DUCKS WINGS FLAPS
+        setInterval(function () {
+            $newDuck.classList.toggle("flap");
+        }, 200);
+    };
 
-//     //FUNCTION TO MOVE THE DUCK
-//     moveDuck = () => {
-//         // animateDuck('.duck');
-//         // CREATE RANDOM MOVES
-//         let left = Math.random() * window.innerWidth
-//         let top  = Math.random() * window.innerHeight
-//         //method returns the width of the element, including left and right padding, in pixels.
-//         newDuck.style.left = left + 'px';
-//         newDuck.style.top = top + 'px'; //pixels
+    //FUNCTION TO MOVE THE DUCK
+    moveDuck = () => {
+        // animateDuck('.duck');
+        // CREATE RANDOM MOVES
+        let left = Math.random() * window.innerWidth
+        let top  = Math.random() * window.innerHeight
+        //method returns the width of the element, including left and right padding, in pixels.
+        newDuck.style.left = left + 'px';
+        newDuck.style.top = top + 'px'; //pixels
 
-//     };
+    };
 
-//     setInterval(moveDuck, 1000);
-//     return newDuck;
+    setInterval(moveDuck, 1000);
+    return newDuck;
 
-//     //FUNCTION THAT CHECK THE NUMBERS OF DUCKS
-//     const checkForWinner = () => {
-//         const numDucks = $('duck')
-//         console.log(numDucks);
-//         if (numDucks.length === 0) {
-//             const $win = $('<div>').addClass('win').text('U WIN');
-//             $('body').append($win)
-//         }
-//     }
-
-
-//     //SHOT FUNCTION
-//     // ADDs A CLASS MANIPUMLATE IT FROMM CSS AND REMOVE THE ORIGINAL CLASS
-//     const addShot = (e) => {
-//         e.target.classList.add('shot'); //use classlist to add and toggle a CSS class
-//         setTimeout(function () {
-//             e.target.remove() //remove duck class after it been clicked
-//             checkForWinner()
-//         }, 1000);
-//     };
-
-//     //CREATE NEW DUCKS 
-//     for (let i = 0; i < 5; i++) {
-//         createDuck().click('click', addShot);
-//     }
+    //FUNCTION THAT CHECK THE NUMBERS OF DUCKS
+    const checkForWinner = () => {
+        const numDucks = $('duck')
+        console.log(numDucks);
+        if (numDucks.length === 0) {
+            const $win = $('<div>').addClass('win').text('U WIN');
+            $('body').append($win)
+        }
+    }
 
 
-// });
+    //SHOT FUNCTION
+    // ADDs A CLASS MANIPUMLATE IT FROMM CSS AND REMOVE THE ORIGINAL CLASS
+    const addShot = (e) => {
+        e.target.classList.add('shot'); //use classlist to add and toggle a CSS class
+        setTimeout(function () {
+            e.target.remove() //remove duck class after it been clicked
+            checkForWinner()
+        }, 1000);
+    };
+
+    //CREATE NEW DUCKS 
+    for (let i = 0; i < 5; i++) {
+        createDuck().click('click', addShot);
+    }
+
+
+});
 
 
 
@@ -70,55 +70,104 @@
 
 
 window.onload = function () {
-console.log('yoooo');
-console.log('h1')
+    const body = document.body;
 
-    const body = $('body');
     console.log(body);
 
 
+
+
     const createDuck = () => {
-        const newDiv = document.createElement("div"); // const $newDiv = $('<div>').addClass('duck')
-        newDiv.setAttribute("class", "duck");         
-        document.body.appendChild(newDiv);           // $('body').append($newDiv)
-
+        let newDuck = document.createElement('div');
+        newDuck.classList.add('duck');
+        document.body.appendChild(newDuck);
+        // let flap = document.querySelector('.flap')
+        // console.log(flap)
         setInterval(function () {
-            newDiv.classList.toggle("flap");
-        }, 200);
+            newDuck.classList.toggle('flap');
+        }, 250);
 
 
-        //MOVE DUCK FUNCTION
+
+
         const moveDuck = () => {
-            let left = Math.random() * window.innerWidth;
-            let top  = Math.random() * window.innerHeight;
-            newDiv.style.left = left + "px";
-            newDiv.style.top  = top + "px";
-        };
+            let left = Math.random() * window.innerWidth
+            let top = Math.random() * window.innerHeight
+            newDuck.style.left = left + 'px'
+            newDuck.style.top = top + 'px'
+        }
+        setInterval(moveDuck, 1000);
 
-        moveDuck();
-        setInterval(moveDuck, 500);
-
-
-        newDiv.addEventListener("click", () => {
-            newDiv.classList.add("shot");
-            setTimeout(() => {
-                newDiv.remove();
-                checkForWinner();
-            }, 700);
-        });
+        return newDuck
     };
 
     const checkForWinner = () => {
-        let ducksRemaining = document.getElementsByClassName("duck");
-        console.log(ducksRemaining.length);
-        if (ducksRemaining.length === 0) {
-             const $win = $('<div>').addClass('win').text('U WIN');
-             $('body').append($win);
+        const numDucks = document.getElementsByClassName('duck')
+        console.log(numDucks)
+        if (numDucks.length === 0) {
+            // alert('U R A ASS DONT KILL ANIMALS')
+            const $win = $('<div>').addClass('win').text('U WIN');
+            $('body').append($win);
         }
-    };
+    }
+
+
+    const addShot = (e) => {
+        e.target.classList.add('shot')
+        setTimeout(function () {
+            e.target.remove()
+            checkForWinner()
+        }, 1000);
+
+    }
+
     for (let i = 0; i < 1; i++) {
-        createDuck();
-    };
+        createDuck().addEventListener('click', addShot);
+    }
+
+
+
+    //TIMER FUNCTION
+    // let counter = 0;
+    // let timeLeft = 30;
+
+    // function setup() {
+    //     noCanvvas();
+
+    //     let timer = select('#timer');
+    //     timer.html(timeleft - currentTime);
+
+
+    //     function timeIt() {
+    //         counter++;
+    //         timer.html(timeleft - counter);
+    //     }
+    //     setInterval(timeIt, 1000);
+    // }
+
+
+    const startingTime = 0.25;
+    let time = startingTime * 60;
+
+    const countdownEl = document.getElementById('countdown');
+
+
+    setInterval(updateTimer, 1000);
+
+    function updateTimer() {
+        const minutes = Math.floor(time / 60);
+        let seconds = time % 60;
+
+
+        second = seconds < 0.50 ? '0' + seconds : seconds;
+
+
+        countdownEl.innerHTML = `${minutes}: ${seconds}`;
+        time--;
+        time = time < 0 ? 0 : time; //AVOID NEGATIVE NUMBERS.
+    }
+
+
+
+
 };
-
-
